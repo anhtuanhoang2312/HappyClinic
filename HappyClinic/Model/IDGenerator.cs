c0f5605c2dca77b60a5ad11719a0db42e906202a
hoanganhtuan <38556283+anhtuanhoang2312@users.noreply.github.com>
2021-02-05 14:35:29 +0700

    Added DataProvider & IDGenerator class

---
+++ HappyClinic/Model/IDGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyClinic.Model
+{ 
+    class IDGenerator
+    {
+        public static string createID(string prefix)
+        {
+            var count = 0;
+
+            if (prefix == "BN")
+            {
+                var table = DataProvider.Instance.DB.Patients;
+                if (table.Count() > 0)
+                {
+                    count = int.Parse(table.ToList()[table.Count() - 1].ID.ToString().Substring(2, 3));
+                }
+            }
+            else if (prefix == "PK")
+            {
+                var table = DataProvider.Instance.DB.ExaminationForms;
+                if (table.Count() > 0)
+                {
+                    count = int.Parse(table.ToList()[table.Count() - 1].ID.ToString().Substring(2, 3));
+                }
+            }
+            else if (prefix == "HD")
+            {
+                var table = DataProvider.Instance.DB.Invoices;
+                if (table.Count() > 0)
+                {
+                    count = int.Parse(table.ToList()[table.Count() - 1].ID.ToString().Substring(2, 3));
+                }
+            }
+            else if (prefix == "T")
+            {
+                var table = DataProvider.Instance.DB.Medicines;
+                if (table.Count() > 0)
+                {
+                    count = int.Parse(table.ToList()[table.Count() - 1].ID.ToString().Substring(1, 3));
+                }
+            }
+            else if (prefix == "CD")
+            {
+                var table = DataProvider.Instance.DB.MedicineUsages;
+                if (table.Count() > 0)
+                {
+                    count = int.Parse(table.ToList()[table.Count() - 1].ID.ToString().Substring(2, 3));
+                }
+            }
+            else if (prefix == "DV")
+            {
+                var table = DataProvider.Instance.DB.MedicineUnits;
+                if (table.Count() > 0)
+                {
+                    count = int.Parse(table.ToList()[table.Count() - 1].ID.ToString().Substring(2, 3));
+                }
+            }
+            else if (prefix == "B")
+            {
+                var table = DataProvider.Instance.DB.Diseases;
+                if (table.Count() > 0)
+                {
+                    count = int.Parse(table.ToList()[table.Count() - 1].ID.ToString().Substring(1, 3));
+                }
+            }
+
+            return prefix + (count + 1).ToString("000");
+        }
+    }
+}
